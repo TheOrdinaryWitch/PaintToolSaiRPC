1c6ea47ba73c02f6c95261a5ace20e6756763777
TheOrdinaryWitch <inkyshadow69@gmail.com>
2024-10-14 18:01:17 -0300

    Added Discord integration

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
 import psList from "ps-list";
 import { windowManager } from "node-window-manager";
 import dotenv from 'dotenv';
+import { Client } from "discord-rpc";
 
+const client = new Client({transport:"ipc"})
+const start = new Date();
 dotenv.config();
 
 async function update() {
@@ -22,11 +25,33 @@ async function update() {
                 break;
             }
             if(windowManager.getWindows()[i].getTitle().split(" - ")[0]=="SAI"){
-                window = windowManager.getWindows()[i].getTitle().split('\\').pop();
-                console.log(window);
+                if(windowManager.getWindows()[i].getTitle()!="SAI"){
+                    let saiRemove = windowManager.getWindows()[i].getTitle().replace("SAI - ", "");
+                    window = "Editing: " + saiRemove.split('\\').pop();
+                    console.log(window);
+                    break;
+                }
+                else{
+                    window = "Starting a New Canvas"
+                }
+                
                 break;
             }
         }
     }
+
+    client.setActivity({
+        state: window,
+        startTimestamp: start,
+        largeImageKey: 'sailogo', // Use the image key you uploaded
+        largeImageText: 'Paint Tool Sai',
+    }, app ? app.pid : null)
 }
-update();
+client.on("ready",()=>{
+    console.log("Online");
+    update();
+    setInterval(update,5000);
+});
+
+console.log("Connecting...");
+client.login({clientId:process.env.DISCORD_APP_ID})
